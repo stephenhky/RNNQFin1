537963050abf351ae92c2c03bade41b7db6eb3df
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2016-02-01 17:21:38 -0500

    working on LSTM

--- RNNModels.py
+++ RNNModels.py
@@ -1,1 +1,27 @@
+# adapted from http://christianherta.de/lehre/dataScience/machineLearning/neuralNetworks/LSTM.php
+
+
+import numpy as np
 import theano
+import theano.tensor as T
+
+dtype=theano.config.floatX
+
+# squashing of the gates should result in values between 0 and 1
+# therefore we use the logistic function
+sigma = lambda x: 1 / (1 + T.exp(-x))
+
+# for the other activation function we use the tanh
+act = T.tanh
+
+# sequences: x_t
+# prior results: h_tm1, c_tm1
+# non-sequences: W_xi, W_hi, W_ci, b_i, W_xf, W_hf, W_cf, b_f, W_xc, W_hc, b_c, W_xy, W_hy, W_cy, b_y
+def one_lstm_step(x_t, h_tm1, c_tm1, W_xi, W_hi, W_ci, b_i, W_xf, W_hf, W_cf, b_f, W_xc, W_hc, b_c, W_xy, W_ho, W_cy, b_o, W_hy, b_y):
+    i_t = sigma(theano.dot(x_t, W_xi) + theano.dot(h_tm1, W_hi) + theano.dot(c_tm1, W_ci) + b_i)
+    f_t = sigma(theano.dot(x_t, W_xf) + theano.dot(h_tm1, W_hf) + theano.dot(c_tm1, W_cf) + b_f)
+    c_t = f_t * c_tm1 + i_t * act(theano.dot(x_t, W_xc) + theano.dot(h_tm1, W_hc) + b_c)
+    o_t = sigma(theano.dot(x_t, W_xo)+ theano.dot(h_tm1, W_ho) + theano.dot(c_t, W_co)  + b_o)
+    h_t = o_t * act(c_t)
+    y_t = sigma(theano.dot(h_t, W_hy) + b_y)
+    return [h_t, c_t, y_t]
