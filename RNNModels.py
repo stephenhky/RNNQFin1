608420492471f06a5b21871e4558eec4a9e8da0a
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2016-02-02 14:10:00 -0500

    correct typos in the original codes

--- RNNModels.py
+++ RNNModels.py
@@ -1,6 +1,5 @@
 # adapted from http://christianherta.de/lehre/dataScience/machineLearning/neuralNetworks/LSTM.php
 
-
 import numpy as np
 import theano
 import theano.tensor as T
@@ -17,7 +16,7 @@ act = T.tanh
 # sequences: x_t
 # prior results: h_tm1, c_tm1
 # non-sequences: W_xi, W_hi, W_ci, b_i, W_xf, W_hf, W_cf, b_f, W_xc, W_hc, b_c, W_xy, W_hy, W_cy, b_y
-def one_lstm_step(x_t, h_tm1, c_tm1, W_xi, W_hi, W_ci, b_i, W_xf, W_hf, W_cf, b_f, W_xc, W_hc, b_c, W_xy, W_ho, W_cy, b_o, W_hy, b_y):
+def one_lstm_step(x_t, h_tm1, c_tm1, W_xi, W_hi, W_ci, b_i, W_xf, W_hf, W_cf, b_f, W_xc, W_hc, b_c, W_xo, W_ho, W_co, b_o, W_hy, b_y):
     i_t = sigma(theano.dot(x_t, W_xi) + theano.dot(h_tm1, W_hi) + theano.dot(c_tm1, W_ci) + b_i)
     f_t = sigma(theano.dot(x_t, W_xf) + theano.dot(h_tm1, W_hf) + theano.dot(c_tm1, W_cf) + b_f)
     c_t = f_t * c_tm1 + i_t * act(theano.dot(x_t, W_xc) + theano.dot(h_tm1, W_hc) + b_c)
@@ -25,3 +24,4 @@ def one_lstm_step(x_t, h_tm1, c_tm1, W_xi, W_hi, W_ci, b_i, W_xf, W_hf, W_cf, b_
     h_t = o_t * act(c_t)
     y_t = sigma(theano.dot(h_t, W_hy) + b_y)
     return [h_t, c_t, y_t]
+
