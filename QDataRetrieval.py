6f58d4e3371c4de950a24c1bd03b74a9ab10f0ea
stephenhky <cwin31>
2016-03-03 21:18:48 -0500

    changed data unit

--- QDataRetrieval.py
+++ QDataRetrieval.py
@@ -13,6 +13,9 @@ class QuanDLRetriever:
         delta_t = np.array(map(lambda t: t.item()/1e+9, delta_t))
         return delta_t
 
+    def getyears(self):
+        return self.getseconds() / (3600*24*365.24)
+
     def getclose(self):
         return np.array(self.data['Close'])
 
