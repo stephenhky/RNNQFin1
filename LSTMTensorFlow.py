b2ecfbf870aea978557c3c76bbaa6a576ec0c39a
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2016-04-07 14:32:01 -0400

    working on tensorflow

--- LSTMTensorFlow.py
+++ LSTMTensorFlow.py
@@ -5,20 +5,19 @@ batch_size = 100
 num_steps = 10
 lstm_size = 100
 
+# Placeholder for the inputs in a given iteration.
+words = tf.placeholder(tf.int32, [batch_size, num_steps])
+
 lstm = rnn_cell.BasicLSTMCell(lstm_size)
 # Initial state of the LSTM memory.
-state = tf.zeros([batch_size, lstm.state_size])
+initial_state = state = tf.zeros([batch_size, lstm.state_size])
 
-loss = 0.0
-for current_batch_of_words in words_in_dataset:
+for i in range(len(num_steps)):
     # The value of state is updated after processing each batch of words.
-    output, state = lstm(current_batch_of_words, state)
+    output, state = lstm(words[:, i], state)
 
-    # The LSTM output can be used to make next word predictions
-    logits = tf.matmul(output, softmax_w) + softmax_b
-    probabilities = tf.nn.softmax(logits)
-    loss += loss_function(probabilities, target_words)
+    # The rest of the code.
+    # ...
 
+final_state = state
 
-
-words = tf.placeholder(tf.int32, [batch_size, num_steps])
