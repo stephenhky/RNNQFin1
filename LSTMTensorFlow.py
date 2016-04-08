a2bf792c59f0454b63a9e81ab6351ca5d82eff04
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2016-04-08 17:09:07 -0400

    progress

--- LSTMTensorFlow.py
+++ LSTMTensorFlow.py
@@ -6,6 +6,9 @@ from nltk.corpus import brown
 batch_size = 100
 num_steps = 10
 lstm_size = 100
+vocab_size = 10000
+size = 1000
+
 
 # Placeholder for the inputs in a given iteration.
 words = tf.placeholder(tf.int32, [batch_size, num_steps])
@@ -13,6 +16,7 @@ words = tf.placeholder(tf.int32, [batch_size, num_steps])
 lstm = rnn_cell.BasicLSTMCell(lstm_size)
 # Initial state of the LSTM memory.
 initial_state = state = tf.zeros([batch_size, lstm.state_size])
+targets = tf.placeholder(tf.int32, [batch_size, num_steps])
 
 for i in range(len(num_steps)):
     # The value of state is updated after processing each batch of words.
@@ -23,7 +27,14 @@ for i in range(len(num_steps)):
 
 final_state = state
 
-
+# functions defined
+softmax_w = tf.get_variable("softmax_w", [size, vocab_size])
+softmax_b = tf.get_variable("softmax_b", [vocab_size])
+logits = tf.matmul(output, softmax_w) + softmax_b
+loss = tf.nn.seq2seq.sequence_loss_by_example(
+    [logits],
+    [tf.reshape(targets, [-1])],
+    [tf.ones([batch_size * num_steps])])
 
 # A numpy array holding the state of LSTM after each batch of words.
 numpy_state = initial_state.eval()
